710c4a853ad3b4d5779568d9209edc6b3abe2079
Franco Monti Venesia <francovenesia05@gmail.com>
2025-07-08 05:40:07 -0300

    Sección Mis certificados 100% completada, agregado de animación al carrusel | 23

--- js/main.js
+++ js/main.js
@@ -58,32 +58,3 @@ window.addEventListener("DOMContentLoaded", () => {
     mobileThemeIcon.src = "assets/icons/moon.svg";
   }
 });
-
-  const track = document.querySelector('.carousel-track');
-  const items = document.querySelectorAll('.carousel-item');
-  const prevBtn = document.querySelector('.carousel-btn.prev');
-  const nextBtn = document.querySelector('.carousel-btn.next');
-
-  let index = 0;
-
-  function updateCarousel() {
-    const itemWidth = items[0].offsetWidth + 32; // 32px por el gap (2rem)
-    track.style.transform = `translateX(-${index * itemWidth}px)`;
-  }
-
-  nextBtn.addEventListener('click', () => {
-    if (index < items.length - 1) {
-      index++;
-      updateCarousel();
-    }
-  });
-
-  prevBtn.addEventListener('click', () => {
-    if (index > 0) {
-      index--;
-      updateCarousel();
-    }
-  });
-
-  window.addEventListener('resize', updateCarousel);
-
